eddcc30a698e0ac5e36b9415e787d496cfbaec78
YB970902 <78996787+YB970902@users.noreply.github.com>
2023-09-27 00:12:38 +0900

    기능 추가 - 에이전트에 도착하기 행동 추가

--- Assets/Scripts/SteeringBehaviour/Agent.cs
+++ Assets/Scripts/SteeringBehaviour/Agent.cs
@@ -60,5 +60,28 @@ namespace SteeringBehaviour
             var desiredVelocity = (Position - _target).normalized * maxSpeed;
             return desiredVelocity - velocity;
         }
+
+        /// <summary>
+        /// 도착하기 행동
+        /// </summary>
+        private Vector2 Arrive(Vector2 _target)
+        {
+            // 목표지점과 목표지점까지의 거리를 계산한다.
+            var toTarget = _target - Position;
+            var dirToTarget = toTarget.normalized;
+            var dist = toTarget.magnitude;
+
+            // 목적지에 도착했으면 힘이 발생하지 않는다.
+            if (dist < float.Epsilon) return Vector2.zero;
+
+            // 감속을 위한 상수.
+            const float decelerationTweaker = 0.1f;
+            
+            // 목적지와 멀수록 속도가 크게 줄어들다가 가까워질수록 조금씩 줄어든다.
+            var speed = Mathf.Min(dist / decelerationTweaker, maxSpeed);
+            var desiredVelocity = dirToTarget * speed;
+
+            return desiredVelocity - velocity;
+        }
     }
 }
